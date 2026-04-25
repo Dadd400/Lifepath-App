151071df5dcecf8b54bca8079faff61484396907
Dadd400 <rjaylin529@gmail.com>
2026-04-25 14:32:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/landing.jsx'
-import CareerQuiz from './pages/CareerQuiz.jsx'
-import CityFinder from './pages/CityFinder.jsx'
-import GlobalCityFinder from './pages/GlobalCityFinder.jsx'
-import CollegeQuiz from './pages/CollegeQuiz.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/quiz/career" element={<CareerQuiz />} />
-        <Route path="/quiz/city-us" element={<CityFinder />} />
-        <Route path="/quiz/city-global" element={<GlobalCityFinder />} />
-        <Route path="/quiz/college" element={<CollegeQuiz />} />
       </Routes>
     </BrowserRouter>
   )
